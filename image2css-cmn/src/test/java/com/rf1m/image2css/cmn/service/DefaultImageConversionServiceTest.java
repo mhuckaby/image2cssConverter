c19ec00f5ce9d838d9d04c50c2cce7d28c971d90
mhuckaby <matthew.huckaby@gmail.com>
2013-06-18 07:43:28 -0500

    I2C-0009 - mhuckaby : added copyright and license text to classes

--- image2css-cmn/src/test/java/com/rf1m/image2css/cmn/service/DefaultImageConversionServiceTest.java
+++ image2css-cmn/src/test/java/com/rf1m/image2css/cmn/service/DefaultImageConversionServiceTest.java
@@ -1,3 +1,21 @@
+/**
+ *
+ * Copyright (c) 2011 Matthew D Huckaby. All rights reservered.
+ * ------------------------------------------------------------------------------------
+ * Image2Css is licensed under Apache 2.0, please see LICENSE file.
+ *
+ * Use of this software indicates you agree to the following as well :
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * This product includes software developed by The Apache Software Foundation (http://www.apache.org/).
+ * ------------------------------------------------------------------------------------
+ */
 package com.rf1m.image2css.cmn.service;
 
 import com.rf1m.image2css.cmn.domain.CssClass;
