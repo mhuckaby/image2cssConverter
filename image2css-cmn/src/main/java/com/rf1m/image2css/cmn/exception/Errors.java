bcfa24482024d104f8b6b595f6b9a7516acbbfe3
mhuckaby <matthew.huckaby@gmail.com>
2013-07-20 01:04:44 -0500

    I2C-GEN-REF - mhuckaby : removed unused enum members

--- image2css-cmn/src/main/java/com/rf1m/image2css/cmn/exception/Errors.java
+++ image2css-cmn/src/main/java/com/rf1m/image2css/cmn/exception/Errors.java
@@ -5,16 +5,12 @@ public enum Errors {
     parametersObjectMustHaveValidImageInputFileOrDir("Image input file or directory must be specified"),
     parametersObjectOutputInvalid("At least one output type required: CSS file to write or output to screen"),
     parametersObjectImageInputFileOrDirNotExists("A valid output to file or screen must be specified for CSS"),
-    parameterBeanType("Unknown bean type, %1$s"),
     parameterHtmlIndexWithNoCssFile("CSS file output is required to produce HTML index"),
 
     parameterCssClassCollectionIsNull("CSS classes collection is null"),
     parameterUnsupportedImageType("Unsupported image type specified"),
 
     fileNotFound("File not found"),
-
-
-    failureToOutput("Failure occurred during output"),
 
     errorReadingFile("Error while reading file"),
     errorClosingFile("Error while closing file");
