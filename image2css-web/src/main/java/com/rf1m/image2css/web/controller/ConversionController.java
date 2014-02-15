67c28d600bd9e4a35319b224ff13c9abf635a565
mhuckaby <matthew.huckaby@gmail.com>
2014-02-15 15:32:47 -0600

    I2C-0012 - mhuckaby : exposed conversion service as rest endpoint /image2css/c/convert?url=$ .. added script for build/debug

---
+++ image2css-web/src/main/java/com/rf1m/image2css/web/controller/ConversionController.java
@@ -0,0 +1,31 @@
+package com.rf1m.image2css.web.controller;
+
+import com.rf1m.image2css.cmn.domain.CssClass;
+import com.rf1m.image2css.cmn.service.ImageConversionService;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+
+@Controller
+public class ConversionController {
+    protected static final Logger logger = LoggerFactory.getLogger(ConversionController.class);
+
+
+    @Autowired
+    ImageConversionService imageConversionService;
+
+    @RequestMapping(value = "/convert", produces = {APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public CssClass convertUrl(@RequestParam final String url) {
+        logger.debug("convertUrl=" + url);
+        return this.imageConversionService.convertUrlAsString(url);
+    }
+}
