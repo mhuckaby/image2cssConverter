77ddd17560dda2c0b7a0a0763c6172785154d53e
mhuckaby <matthew.huckaby@gmail.com>
2014-02-16 00:23:23 -0600

    I2C-0012 - mhuckaby : optimized imports

--- image2css-web/src/main/java/com/rf1m/image2css/web/controller/ConversionController.java
+++ image2css-web/src/main/java/com/rf1m/image2css/web/controller/ConversionController.java
@@ -2,10 +2,9 @@ package com.rf1m.image2css.web.controller;
 
 import com.rf1m.image2css.cmn.domain.CssClass;
 import com.rf1m.image2css.cmn.service.ImageConversionService;
-import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
